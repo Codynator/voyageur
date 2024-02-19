3375cdc61ffed2d420c435a107e55eb1ee8b5c88
Codynator <113552830+Codynator@users.noreply.github.com>
2024-02-19 20:12:21 +0100

    Styling of header-container elements and minor improvments

--- src/main.php
+++ src/main.php
@@ -14,9 +14,9 @@
 <body>
     <nav>
         <div class="left-nav">
-            <a href="./main.html" class="logo"><img src="../public/voyageur_logo.png" alt=""></a>
-            <a href="./main.html">All Inclusive</a>
-            <a href="./main.html">Last Minute</a>
+            <a href="./main.php" class="logo"><img src="../public/voyageur_logo.png" alt=""></a>
+            <a href="./main.php">All Inclusive</a>
+            <a href="./main.php">Last Minute</a>
             <!-- <progress value="25" max="100"></progress> -->
         </div>
         <div class="right-nav">
@@ -34,9 +34,9 @@
                             <path fill="var(--text)" d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m0 2a8 8 0 0 1 8 8a8 8 0 0 1-8 8z" />
                         </svg>
                     </button>
-                    <a href="./main.html">Contact</a>
-                    <a href="./main.html">Favorites</a>
-                    <a href="./main.html">Log in</a>
+                    <a href="./main.php">Contact</a>
+                    <a href="./main.php">Favorites</a>
+                    <a href="./main.php">Log in</a>
                 </div>
             </div>
         </div>
@@ -47,18 +47,31 @@
             <h2>Where would like to go?</h2>
 
             <div class="header-container">
+                <div class="button-container">
+                    <button>Select destination</button>
+                    <div class="button-menu"></div>
+                </div>
+                <div class="button-container">
+                    <button>From where and how</button>
+                    <div class="button-menu"></div>
+                </div>
+                <div class="button-container">
+                    <button>For how long</button>
+                    <div class="button-menu"></div>
+                </div>
+                <div class="button-container">
+                    <button>For how many people</button>
+                    <div class="button-menu"></div>
+                </div>
+
                 <form method="POST" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                    <button type="button">Destination</button>
-                    <button type="button">From where</button>
-                    <button type="button">For how long</button>
-                    <button type="button">For how many people</button>
                     <input type="submit" value="Search" name="search-btn">
 
-                    <input type="text" name="trevel-length">
-                    <input type="text" name="ammount-of-adults">
-                    <input type="text" name="ammount-of-children">
-                    <input type="text" name="destination">
-                    <input type="text" name="airport">
+                    <input type="text" name="trevel-length" hidden>
+                    <input type="text" name="ammount-of-adults" hidden>
+                    <input type="text" name="ammount-of-children" hidden>
+                    <input type="text" name="destination" hidden>
+                    <input type="text" name="airport" hidden>
                 </form>
             </div>
         </div>
@@ -67,6 +80,7 @@
     <main>
 
     </main>
+
     <script src="./scripts/themeChanger.js"></script>
     <script src="./scripts/searchEngine.js"></script>
 </body>
