ea00d10ce3cd9ab3e562b57a9f33fe7ecc5e659a
Codynator <113552830+Codynator@users.noreply.github.com>
2024-03-18 22:23:51 +0100

    Minor issue fix

--- src/scripts/searchEngine.js
+++ src/scripts/searchEngine.js
@@ -1,14 +1,4 @@
 const headerContainer = document.getElementsByClassName('header-container')[0];
 const buttons = headerContainer.querySelectorAll('button');
-console.log(buttons);
 const inputs = headerContainer.querySelectorAll('input');
 
-for (let i = 0; i < buttons.length; i++) {
-    console.log(buttons[i]);
-    console.log(inputs[i+1]);
-
-    buttons[i].addEventListener('click', (event) => {
-        event.preventDefault();
-        inputs[i+1].value = `Lorem ${i}`;
-    });
-}
